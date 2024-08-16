6e4ee217c11e8ed9ef9ec958e94a7b8fa1b47523
EpheyHertz <151906589+EpheyHertz@users.noreply.github.com>
2024-08-16 22:51:38 +0300

    1st Update settings.py

--- aiblog_app/settings.py
+++ aiblog_app/settings.py
@@ -93,6 +93,7 @@ DATABASES = {
         'PORT':'3307',
         'OPTIONS': {
             'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
         },
     }
 }
